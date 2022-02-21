b76bf899815c2f8352717aa8ff3b96aca9a075f0
Ali Hardan <ali.hardan76@gmail.com>
2022-02-21 10:56:25 +0330

    .

--- .eleventy.js
+++ .eleventy.js
@@ -42,9 +42,9 @@ async function genFileRows(game) {
     } else if(game.ia_id) {
       result += `<td class="download"><a href="`;
       if (file.tgchannel) {
-        result += `https://t.me/${file.tgchannel}`
+        result += encodeURI(`https://t.me/${file.tgchannel}`)
       } else {
-        result += `https://archive.org/download/${game.ia_id}/${file.file_name}`
+        result += encodeURI(`https://archive.org/download/${game.ia_id}/${file.file_name}`)
       }
       result += `" target="_blank"><svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg> دانلود</a></td>`;
     } else {
