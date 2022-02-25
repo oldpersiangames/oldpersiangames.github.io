4482d69ea8d39171844391dc72089eea9a34bb98
Ali Hardan <ali.hardan76@gmail.com>
2022-02-25 20:10:17 +0330

    .

---
+++ scripts/gameswoid.js
@@ -0,0 +1,13 @@
+const gamesr = require('/archive/tmp/opg/oldpersiangames.github.io/src/_data/games.js');
+var games = null;
+gamesr().then(result => {
+  games = "";
+  for (gameIndex = 0; gameIndex < result.length; gameIndex++) { 
+    if(!result[gameIndex].ia_id && !result[gameIndex].selling && result[gameIndex].tgfiles && result[gameIndex].tgfiles.length && result[gameIndex].id != "mountaineer-hi-vu") {
+      games = games + " " + result[gameIndex].id
+    }
+  }
+  process.stdout.write(games.trim())
+}).catch(err => {
+  console.log(err);
+});
