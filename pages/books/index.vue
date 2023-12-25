83b32116a816bfce945254c182fb99c6ffe7ad3b
alihardan <alihardanc@gmail.com>
2023-12-25 06:49:04 +0330

    update books section, add lohekhaneh page

---
+++ pages/books/index.vue
@@ -0,0 +1,35 @@
+<script setup>
+const localePath = useLocalePath();
+</script>
+<template>
+  <Head>
+    <Title>دانلود کتاب ها و مجلات</Title>
+    <Meta name="description" content="دانلود کتاب ها و مجلات" />
+  </Head>
+
+  <h1
+    class="mt-4 text-center text-xl font-bold leading-none tracking-tight text-gray-900 dark:text-white"
+  >
+    دانلود کتاب ها و مجلات
+  </h1>
+  <div
+    class="mt-4 pb-24 prose prose-primary dark:prose-invert max-w-none font-light"
+    dir="rtl"
+  >
+    <ul>
+      <li>
+        <NuxtLink
+          class="text-primary-600 no-underline font-light hover:underline"
+          :to="localePath('/books/lohekhaneh')"
+          >دانلود کتاب ها و مجلات لوحه خانه</NuxtLink
+        >
+      </li>
+    </ul>
+    <h2>مشارکت</h2>
+
+    <p>
+      می‌تونین کتاب‌های مرتبط رو برامون بفرستین. یا اگه نسخه بهتری از کتاب‌های
+      موجود دارین. روی دکمه‌ی مشارکت بالای سایت کلیک کنین.
+    </p>
+  </div>
+</template>
---
+++ pages/books/lohekhaneh.vue
@@ -0,0 +1,423 @@
+<script setup>
+const localePath = useLocalePath();
+</script>
+<template>
+  <Head>
+    <Title>دانلود کتاب ها و مجلات لوحه خانه</Title>
+    <Meta name="description" content="دانلود کتاب ها و مجلات لوحه خانه" />
+  </Head>
+
+  <h1
+    class="mt-4 text-center text-xl font-bold leading-none tracking-tight text-gray-900 dark:text-white"
+  >
+    دانلود کتاب ها و مجلات لوحه خانه
+  </h1>
+  <div class="text-center font-extralight text-sm mt-2">
+    آخرین بروزرسانی: <span dir="ltr">2023-12-25</span>
+  </div>
+
+  <div
+    class="mt-4 pb-24 prose prose-primary dark:prose-invert max-w-none font-light"
+    dir="rtl"
+  >
+    <div>
+      <h2>مجموعه کامل کد، رمز و استراتژی بازی های پلی استیشن ۱</h2>
+      <figure class="text-center">
+        <img
+          src="/img/lohekhaneh-ps1-book-cover.jpg"
+          alt="مجموعه کامل کد، رمز، واستراتژی بازی های پلی استیشن ۱ لوحه خانه"
+          class="max-w-xl mx-auto"
+        />
+        <figcaption>
+          کتاب کد، رمز و استراتژی بازی های پلی استیشن ۱ - باشگاه نرم افزاری لوحه
+          خانه
+        </figcaption>
+      </figure>
+      <p>این کتاب توسط «لوحه خانه» تهیه و در سال ۱۳۸۲ چاپ شده است.</p>
+      <div>
+        <a
+          href="https://archive.org/download/ps1-lohekhaneh/lohekhaneh-ps1-book.pdf"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود کتاب
+        </a>
+      </div>
+      <div class="mt-4">
+        <a
+          href="https://archive.org/download/ps1-lohekhaneh/lohekhaneh-ps1-book-cover.jpg"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود طرح جلد
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <h2>مجموعه کامل کد، رمز و راه یابی بازی های پلی استیشن ۲</h2>
+      <figure class="text-center">
+        <img
+          src="/img/ps2-book.jpg"
+          alt="مجموعه کامل کد، رمز و راه یابی بازی های پلی استیشن ۲ لوحه خانه"
+          class="max-w-sm mx-auto"
+        />
+        <figcaption>
+          کتاب کد، رمز و راه یابی بازی های پلی استیشن ۲ - باشگاه نرم افزاری لوحه
+          خانه
+        </figcaption>
+      </figure>
+      <p>این کتاب توسط «لوحه خانه» تهیه و در سال ۱۳۸۷ چاپ شده است.</p>
+      <div>
+        <a
+          href="https://archive.org/download/ps2-lohekhaneh/lohekhaneh-ps2-book.pdf"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود کتاب
+        </a>
+      </div>
+      <div class="mt-4">
+        <a
+          href="https://archive.org/download/ps2-lohekhaneh/lohekhaneh-ps2-book-cover.jpg"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود طرح جلد
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <h2>مجموعه کد، رمز و راهنمایی بازی های کامپیوتر (جلد ۱ و ۲)</h2>
+      <figure class="text-center">
+        <img
+          src="/img/lohekhaneh-pc-1-2-book-cover.jpg"
+          alt="مجموعه کد، رمز و راهنمایی بازی های کامپیوتر (جلد ۱ و ۲)"
+          class="max-w-xl mx-auto"
+        />
+        <figcaption>
+          مجموعه کد، رمز و راهنمایی بازی های کامپیوتر (جلد ۱ و ۲) - باشگاه نرم
+          افزاری لوحه خانه
+        </figcaption>
+      </figure>
+      <div>
+        <a
+          href="https://archive.org/download/pc-1-2-lohekhaneh/lohekhaneh-pc-1-2-book.pdf"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود کتاب
+        </a>
+      </div>
+      <div class="mt-4">
+        <a
+          href="https://archive.org/download/pc-1-2-lohekhaneh/lohekhaneh-pc-1-2-book-cover.jpg"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود طرح جلد
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <h2>مجموعه کد، رمز و راهنمایی بازی های کامپیوتر (جلد ۳)</h2>
+      <figure class="text-center">
+        <img
+          src="/img/lohekhaneh-pc-3-book-cover.jpg"
+          alt="مجموعه کد، رمز و راهنمایی بازی های کامپیوتر (جلد ۳)"
+          class="max-w-xl mx-auto"
+        />
+        <figcaption>
+          مجموعه کد، رمز و راهنمایی بازی های کامپیوتر (جلد ۳) - باشگاه نرم
+          افزاری لوحه خانه
+        </figcaption>
+      </figure>
+      <div>
+        <a
+          href="https://archive.org/download/pc-3-lohekhaneh/lohekhaneh-pc-3-book.pdf"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود کتاب
+        </a>
+      </div>
+      <div class="mt-4">
+        <a
+          href="https://archive.org/download/pc-3-lohekhaneh/lohekhaneh-pc-3-book-cover.jpg"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود طرح جلد
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <h2>مجموعه کامل رمزهای سگا</h2>
+      <figure class="text-center">
+        <img
+          src="/img/lohekhaneh-sega-book-cover.jpg"
+          alt="مجموعه کامل رمزهای سگا"
+          class="max-w-xl mx-auto"
+        />
+        <figcaption>
+          مجموعه کامل رمزهای سگا - باشگاه نرم افزاری لوحه خانه
+        </figcaption>
+      </figure>
+      <div>
+        <a
+          href="https://archive.org/download/sega-lohekhaneh/lohekhaneh-sega-book.pdf"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود کتاب
+        </a>
+      </div>
+      <div class="mt-4">
+        <a
+          href="https://archive.org/download/sega-lohekhaneh/lohekhaneh-sega-book-cover.jpg"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود طرح جلد
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <h2>مجموعه کد، رمز و راه یابی بازی های ایکس باسک ۳۶۰</h2>
+      <figure class="text-center">
+        <img
+          src="/img/lohekhaneh-xbox360-book-cover.jpg"
+          alt="مجموعه کد، رمز و راه یابی بازی های ایکس باسک ۳۶۰"
+          class="max-w-xl mx-auto"
+        />
+        <figcaption>
+          مجموعه کد، رمز و راه یابی بازی های ایکس باسک ۳۶۰ - باشگاه نرم افزاری
+          لوحه خانه
+        </figcaption>
+      </figure>
+      <div>
+        <a
+          href="https://archive.org/download/xbox360-lohekhaneh/lohekhaneh-xbox360-book.pdf"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود کتاب
+        </a>
+      </div>
+      <div class="mt-4">
+        <a
+          href="https://archive.org/download/xbox360-lohekhaneh/lohekhaneh-xbox360-book-cover.jpg"
+          target="_blank"
+          class="text-primary-600 no-underline font-light hover:underline"
+        >
+          <UIcon
+            class="w-6 h-6 text-primary-600 align-middle"
+            name="i-heroicons-inbox-arrow-down"
+          />
+          دانلود طرح جلد
+        </a>
+      </div>
+    </div>
+
+    <h2>مشارکت‌کنندگان</h2>
+    <p>
+      دوستمون (<NuxtLink
+        class="text-primary-600 hover:underline font-medium"
+        dir="ltr"
+        :to="{
+          path: localePath('/contributors'),
+          hash: '#Nofuzy',
+        }"
+        >@Nofuzy</NuxtLink
+      >) موفق شده ارتباط مستقیم با سردبیر و نویسنده اصلی مجموعه مجلات «لوحه
+      خانه» (یعنی جناب
+      <strong class="text-primary-600">محمدرضا شفیعی علویجه</strong>) برقرار
+      کنه. هرچند سال‌های زیادی از آخرین انتشار اون مجلات می‌گذره و به کلی این
+      فعالیت متوقف شده، ایشون موافقت کردن که کتاب‌ها و مجلات قدیمیشون رو مجددا
+      بصورت دیجیتال منتشر کنیم و فایل‌های خام رو در اختیار ما قرار دادن.
+    </p>
+    <div
+      class="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 inline w-4 h-4 mr-3"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+        />
+      </svg>
+      <span class="sr-only">Info</span>
+      <div>
+        بدیهیه که هرگونه استفاده تجاری و غیرمتعارف از این آثار مورد رضایت سردبیر
+        و دست‌اندرکاران نیست.
+      </div>
+    </div>
+    <p>
+      تشکر از مشارکت‌کنندگان سابق که قبل از اینکه فایل‌ها از سردبیر گرفته بشه،
+      دوتا از کتاب‌ها رو اسکن کرده بودن:
+    </p>
+    <p>
+      <NuxtLink
+        class="text-primary-600 hover:underline font-medium"
+        dir="ltr"
+        :to="{ path: localePath('/contributors'), hash: '#irmkh13' }"
+        >@irmkh13</NuxtLink
+      >
+      بابت آغاز این حرکت و در اختیار گذاشتن کتاب پلی استیشن ۱،
+      <NuxtLink
+        class="text-primary-600 hover:underline font-medium"
+        dir="ltr"
+        :to="{ path: localePath('/contributors'), hash: '#aqmoein' }"
+        >@aqmoein</NuxtLink
+      >
+      بابت در اختیار گذاشتن کتاب پلی استیشن ۲،
+      <NuxtLink
+        class="text-primary-600 hover:underline font-medium"
+        dir="ltr"
+        :to="{
+          path: localePath('/contributors'),
+          hash: '#alihardan',
+        }"
+        >@alihardan</NuxtLink
+      >
+      بابت اسکن کامل کتاب های پلی استیشن ۱ و ۲،
+      <NuxtLink
+        class="text-primary-600 hover:underline font-medium"
+        dir="ltr"
+        :to="{
+          path: localePath('/contributors'),
+          hash: '#AG_e_e_kA',
+        }"
+        >@AG_e_e_kA</NuxtLink
+      >
+      بابت تدوین و منظم‌سازی فایل‌های اسکن شده و تذکرات کیفی و طراحی مجدد طرح
+      جلد کتاب‌ها
+    </p>
+    <h2>نواقص</h2>
+    <div
+      class="max-w-2xl mx-auto flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 inline w-4 h-4 me-3"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+        />
+      </svg>
+      <span class="sr-only">Info</span>
+      <div>
+        <ul class="marker:text-red-600 m-0">
+          <li>
+            فایل مجلات لوحه‌خانه (غیر از کتاب‌ها هستن) رو در اختیار نداریم. اگر
+            فایل رو بصورت اصلی و یا اسکن شده دارید، برای ما ارسال کنید.
+          </li>
+          <li>
+            فایل چاپ‌های دیگر کتاب راهنمای پلی‌استیشن ۲ که در چند جلد منتشر بود
+            رو در اختیار نداریم.
+          </li>
+          <li>طرح جلد اصلی کتاب پلی‌استیشن ۲ رو در اختیار نداریم.</li>
+          <li>فایل جلد چهارم راهنمای بازی های کامپیوتر رو در اختیار نداریم.</li>
+          <li>...؟</li>
+        </ul>
+      </div>
+    </div>
+    <h2>
+      تاریخچه و سرگذشت لوحه خانه به قلم محمدرضا شفیعی علویجه م. 1338 سردبیر لوحه
+      خانه
+    </h2>
+    <div class="max-w-2xl mx-auto">
+      <p>
+        شروع کار لوحه خانه در سال 1370 با دایر کردن کلوپ بازی با یک دستگاه کنسول
+        میکرو در زیر پله ای ده متری واقع در میدان هاشمی خیابان پارساییان بود که
+        با آمدن سگا، این کنسول نیز به آن اضافه شد. بعد از مدت کوتاهی با توجه به
+        رمزهایی که می توانست بازی را برای بازیکن آسان و جذابتر کند این رمزها جمع
+        آوری و در دفترچه هایی چند برگی تکثیر و در اختیار علاقمندان قرار گرفت. ده
+        ها جلد دفترچه رمز برای سگا و پلی استیشن 1 به مرور نوشته و تکثیر گردید که
+        پس از مدتی این دفترچه ها در کتابهایی جداگانه به چاپ رسید که اولین کتاب
+        های لوحه خانه بودند.
+      </p>
+      <p>
+        با آمدن کنسول های دیگر، لوحه خانه با جدیت و علاقه بیشتری با بالا بردن
+        کیفیت چاپ کتاب ها چندین جلد کتاب برای پلی استیشن 1 به چاپ رساند. این
+        کتاب ها ابتدا حجم کوچکتری داشت که در چاپ های بعدی به مرور رمز و راه یابی
+        های بازی های جدید را به آن اضافه نمود تا در نهایت با آمدن کنسول پلی
+        استیشن 2 با نام مجموعه کامل رمز بازی های پلی استیشن 1 کار بر روی این
+        کتاب خاتمه یافت که این کتاب بعدها چندین بار تجدید چاپ شد.
+      </p>
+      <p>
+        با آمدن کنسول پلی استیشن 2، لوحه خانه اقدام به دریافت مجوز مجله بازی های
+        رایانه ای نمود که با دریافت مجوز چاپ مجله به صورت ماهنامه، حجم کار تهیه
+        و تنظیم و نشر مطالب بیشتر و سنگینتر گردید و این در حالی است که کنسول های
+        دیگر مانند وی، ایکس باکس، نینتندو، دریمکست و کامپیوتر نیز وارد میدان شده
+        بودند. در این مرحله، لوحه خانه نیاز به مکانی بزرگتر داشت که با نقل مکان
+        به دروازه دولت خیابان خاقانی این مهم نیز تحقق پذیرفت و با همکاری و یاری
+        علاقمندان از سراسر کشور توانست مجله بازیهای رایانه ای را تا 44 شماره
+        ادامه دهد.
+      </p>
+      <p>
+        لوحه خانه در طی دوران فعالیت خود 44 جلد مجله تحت عنوان بازی های رایانه
+        ای به چاپ رساند به انظمام یک جلد کتاب برای سگا، یک جلد برای پلی استیشن
+        1، یک جلد مجموعه کامل برای پلی استیشن 2 که حاصل جمع چندین جلد کتاب برای
+        این کنسول بود، یک جلد برای کنسول ایکس باکس 360، و 4 جلد برای کامپیوتر.
+      </p>
+    </div>
+  </div>
+</template>
